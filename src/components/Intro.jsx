9d27761bd15707b7d041b989eb165c8d9577f5ab
Jon Formantes <36646177+matui3@users.noreply.github.com>
2023-08-01 16:56:59 -0700

    Portfolio Website!

---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Intro() {
+    return (<div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+        <h1 className="text-4x1 md:text-7x1 dark:text-white mb-1 md:mb-3 font-bold">Jon Formantes</h1>
+        <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
+        <p className="text-smal max-w-xl mb-6 font-bold">This is my entire bio</p>
+        </div>)
+}
+
+export default Intro;
---
+++ src/data/portfolio.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        id: 1,
+        title: 'Terminal Website',
+        imgUrl: '\\assets\\terminal-website.jpg',
+        stack: ['HTML', 'CSS', 'JavaSccript'],
+        link: 'https://fkcodes.com',
+    },
+    {
+        id: 2,
+        title: 'Developer Portfolio',
+        imgUrl: '\\assets\\dev-portfolio.jpg',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link: 'https://thehackershideout.com',
+    },
+    {
+        id: 3,
+        title: 'AI Nerf Gun',
+        imgUrl: '\\assets\\ai-nerf-gun.jpg',
+        stack: ['Python', 'C++', 'Robotics'],
+        link: 'https://youtu.be/A8eegxL82PI',
+    }
+]
