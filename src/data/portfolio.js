dc8c7147379db4e7daa22528271d54b1594bc6bd
Jon Formantes <36646177+matui3@users.noreply.github.com>
2023-08-08 10:12:52 -0700

    corrected links

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,20 +4,20 @@ export default [
         title: 'Chess Game',
         imgUrl: '\\assets\\Chess Asset.png',
         stack: ['Python', 'Pygame'],
-        link: '',
+        link: 'https://github.com/matui3/Chess',
     },
     {
         id: 2,
         title: 'Snake Game',
         imgUrl: '\\assets\\Snake Game Asset.png',
         stack: ['JavaScript', 'Canvas'],
-        link: '',
+        link: 'https://github.com/matui3/Snake-JavaScript',
     },
     {
         id: 3,
         title: 'Blog Project',
         imgUrl: '\\assets\\Blog Project.png',
         stack: ['JavaScript', 'MongoDB', 'NodeJS'],
-        link: '',
+        link: 'https://github.com/matui3/Blog-Project',
     }
 ]
